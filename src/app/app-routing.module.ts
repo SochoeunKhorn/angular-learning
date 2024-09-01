e011d44af21217fc6d62fb1dfd193763327b55a0
Sochoeun <sochoeun1202@gmail.com>
2024-09-01 13:44:51 +0700

    init angular-prime

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,8 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
-import {Page1Component} from "./pages/page1/page1.component";
-import {Page2Component} from "./pages/page2/page2.component";
-import {Page3Component} from "./pages/page3/page3.component";
-import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
-import {AppComponent} from "./app.component";
 
 /*== list of route ==*/
-const routes: Routes = [
-  {
-    path: 'page-1',
-    component: Page1Component
-  },
-  {
-    path: 'page-2',
-    component: Page2Component
-  },
-  {
-    path: 'page-3',
-    component: Page3Component
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  },
-  {
-    path: '',
-    component: AppComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
